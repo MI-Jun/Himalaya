54a1bfda3061c64710c140659998e11cace723f4
一坑带四猪 <a904200631@126.com>
2020-12-28 12:43:54 +0800

    Completed indicator.

--- app/src/main/java/com/example/himalaya/MainActivity.java
+++ app/src/main/java/com/example/himalaya/MainActivity.java
@@ -1,53 +1,79 @@
 package com.example.himalaya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.contentpager.content.ContentPager;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
 
+import com.example.himalaya.adapters.IndicatorAdapter;
+import com.example.himalaya.adapters.MainContentAdapter;
+import com.example.himalaya.utils.LogUtil;
 import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
 import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
 import com.ximalaya.ting.android.opensdk.model.category.Category;
 import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
 
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     private static final String tag = "MainActivity";
+    private MagicIndicator mMagicIndicator;
+    private ViewPager mContentPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
-        Map<String, String> map = new HashMap<>();
-        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
-            @Override
-            public void onSuccess(CategoryList categoryList) {
-                List<Category> categories = categoryList.getCategories();
-                if (categories != null) {
-                    int size = categories.size();
-                    Log.d(tag,"categories size --<" + size);
-                    for (Category category : categories){
-                        Log.d(tag,"category --> " + category.getCategoryName());
-                    }
-                }
-            }
-
-            @Override
-            public void onError(int i, String s) {
-                Log.e(tag,"error code -" + i +"error msg -->" + s);
-
-            }
-        });
 
 
     }
 
+    private void initView() {
+        mMagicIndicator = findViewById(R.id.main_indicator);
+        mMagicIndicator.setBackgroundColor(ContextCompat.getColor(this,R.color.main_color));
 
+        //创建indicator的适配器
+        IndicatorAdapter adapter = new IndicatorAdapter(this);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdjustMode(true);
+        commonNavigator.setAdapter(adapter);
+
+        //ViewPager
+        mContentPager = findViewById(R.id.content_pager);
+
+        //创建内容适配器
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        MainContentAdapter mainContentAdapter = new MainContentAdapter(supportFragmentManager);
+
+        mContentPager.setAdapter(mainContentAdapter);
+
+        //把ViewPager和indicator绑定在一起
+        mMagicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(mMagicIndicator, (ViewPager) mContentPager);
+
+
+
+    }
 
 
 }
