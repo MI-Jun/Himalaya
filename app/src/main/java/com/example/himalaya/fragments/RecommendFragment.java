f985f8534c91754c4e699ae4268ec896020a2dc6
一坑带四猪 <a904200631@126.com>
2021-01-20 13:38:04 +0800

    Completed recommed part and refactor it't codes

--- app/src/main/java/com/example/himalaya/fragments/RecommendFragment.java
+++ app/src/main/java/com/example/himalaya/fragments/RecommendFragment.java
@@ -1,17 +1,108 @@
 package com.example.himalaya.fragments;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.himalaya.R;
-import com.example.himalaya.base.BaseFragment;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class RecommendFragment extends BaseFragment {
+import com.example.himalaya.R;
+import com.example.himalaya.adapters.RecommendListAdapter;
+import com.example.himalaya.base.BaseFragment;
+import com.example.himalaya.interfaces.IRecommendViewCallback;
+import com.example.himalaya.presenters.RecommendPresenter;
+import com.example.himalaya.utils.Constants;
+import com.example.himalaya.utils.LogUtil;
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
+
+import net.lucode.hackware.magicindicator.buildins.UIUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RecommendFragment extends BaseFragment implements IRecommendViewCallback {
+    private static final String TAG = "";
+    private View mRootView;
+    private RecyclerView mRecommendRv;
+    private RecommendListAdapter mRecommendListAdapter;
+    private RecommendPresenter mRecommendPresenter;
+
     @Override
     protected View onSubViewLoaded(LayoutInflater layoutInflater, ViewGroup container) {
+        //view加载完成
+        mRootView = layoutInflater.inflate(R.layout.fragment_recommend,container,false);
 
-        View rootView = layoutInflater.inflate(R.layout.fragment_recommend,container,false);
-        return rootView;
+        //RecycleView的使用
+        //1.找到控件
+        mRecommendRv = mRootView.findViewById(R.id.recommend_list);
+        //2.设置布局管理器
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecommendRv.setLayoutManager(linearLayoutManager);
+        mRecommendRv.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                outRect.top = UIUtil.dip2px(view.getContext(),3);
+                outRect.bottom = UIUtil.dip2px(view.getContext(),3);
+                outRect.right = UIUtil.dip2px(view.getContext(),5);
+                outRect.left = UIUtil.dip2px(view.getContext(),5);
+
+            }
+        });
+        //3.设置适配器
+        mRecommendListAdapter = new RecommendListAdapter();
+        mRecommendRv.setAdapter(mRecommendListAdapter);
+
+
+        //获取到逻辑层的对象
+        mRecommendPresenter = RecommendPresenter.getInstance();
+        //先要设置通知接口的注册
+        mRecommendPresenter.registerViewCallback(this);
+        //获取推荐列表
+        mRecommendPresenter.getRecommendList();
+
+        //返回view，给界面显示
+        return mRootView;
+    }
+
+
+
+
+    @Override
+    public void onRecommendListLoaded(List<Album> result) {
+
+        //当我们获取到推荐内容的时候，这个方法就会被调用（成功了）
+        //数据回来以后，就是更新UI了
+        //把数据设置给适配器，并且更新UI
+        mRecommendListAdapter.setData(result);
+
+    }
+
+    @Override
+    public void onLoaderMore(List<Album> result) {
+
+    }
+
+    @Override
+    public void onRefreshMore(List<Album> result) {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        //取消接口的注册，避免内存泄漏
+        if (mRecommendPresenter != null) {
+            mRecommendPresenter.unRegisterViewCallback(this);
+        }
     }
 }
---
+++ app/src/main/java/com/example/himalaya/presenters/RecommendPresenter.java
@@ -0,0 +1,110 @@
+package com.example.himalaya.presenters;
+
+import com.example.himalaya.interfaces.IRecommendPresenter;
+import com.example.himalaya.interfaces.IRecommendViewCallback;
+import com.example.himalaya.utils.Constants;
+import com.example.himalaya.utils.LogUtil;
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RecommendPresenter implements IRecommendPresenter {
+
+    private static final String TAG = "";
+    private RecommendPresenter(){}
+
+    private List<IRecommendViewCallback> mCallbacks = new ArrayList<>();
+    private static RecommendPresenter sInstance = null;
+
+    /**
+     * 获取单例对象
+     * @return
+     */
+    public static RecommendPresenter getInstance() {
+        if (sInstance == null){
+           synchronized (RecommendPresenter.class){
+               if (sInstance == null) {
+                   sInstance = new RecommendPresenter();
+               }
+           }
+        }
+       return sInstance;
+    }
+
+    /**
+     * 获取推荐内容，其实就是猜你喜欢
+     * 接口：喜马拉雅SDK，3.10.6 获取猜你喜欢专辑
+     * */
+    @Override
+    public void getRecommendList() {
+        //获取推荐内容
+        //封装参数
+        Map<String, String> map = new HashMap<>();
+        //这个参数表示一页数据返回多少页（Constants.RECOMMEND_COUNT + ""）
+        map.put(DTransferConstants.LIKE_COUNT, Constants.RECOMMEND_COUNT + "");
+        CommonRequest.getGuessLikeAlbum(map, new IDataCallBack<GussLikeAlbumList>() {
+            @Override
+            public void onSuccess(GussLikeAlbumList gussLikeAlbumList) {
+                LogUtil.d(TAG,"thread name -- >" + Thread.currentThread()..getName());
+                //数据获取成功
+                if (gussLikeAlbumList != null){
+                    List<Album> albumList = gussLikeAlbumList.getAlbumList();
+                    //数据回来后，我们去更新UI
+                    //upRecommendUI(albumList);
+                    handlerRecommendResult(albumList);
+                }
+
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                //数据获取失败
+                LogUtil.d(TAG,"error -- >" + i);
+                LogUtil.d(TAG,"errorMsg -- >" + s);
+
+            }
+        });
+
+    }
+
+
+    private void handlerRecommendResult(List<Album> albumList) {
+        //通知UI更新
+        if (mCallbacks != null) {
+            for (IRecommendViewCallback callback : mCallbacks) {
+                callback.onRecommendListLoaded(albumList);
+            }
+        }
+    }
+
+    @Override
+    public void pull2RefreshMore() {
+
+    }
+
+    @Override
+    public void loadMore() {
+
+    }
+
+    @Override
+    public void registerViewCallback(IRecommendViewCallback callback) {
+        if (mCallbacks != null && mCallbacks.contains(callback)) {
+            mCallbacks.add(callback);
+        }
+    }
+
+    @Override
+    public void unRegisterViewCallback(IRecommendViewCallback callback) {
+        if (mCallbacks != null) {
+            mCallbacks.remove(mCallbacks);
+        }
+    }
+}
